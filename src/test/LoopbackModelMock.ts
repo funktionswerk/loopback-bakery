44cf709ad739b5847331edab78ac62c6b0151131
Kay Wolter <kay.wolter@codeva.de>
2017-02-04 12:59:43 +0100

    Check created record instance

--- src/test/LoopbackModelMock.ts
+++ src/test/LoopbackModelMock.ts
@@ -1,9 +1,10 @@
 
 export default class LoopbackModelMock {
+  public nextId: number;
 
   public create(data: any, cb: (err: Error, record: any) => void) {
     process.nextTick(() => {
-      cb(null, data);
+      cb(null, {id: this.nextId, ...data});
     });
   }
 
--- src/test/Recipe.spec.ts
+++ src/test/Recipe.spec.ts
@@ -12,14 +12,18 @@ describe('Recipe', () => {
   beforeEach(() => {
     model = new LoopbackModelMock();
     sinon.stub(model, 'create', model.create);
+    model.nextId = 5;
   });
 
   describe('make', () => {
 
     it('should create a new model with the passed attributes', async () => {
       let recipe = bakery.Recipe(model);
-      await recipe({name: 'Steven', email: 'steven@mail.test'});
+      const record = await recipe({name: 'Steven', email: 'steven@mail.test'});
       sinon.assert.alwaysCalledWithExactly(model.create, {name: 'Steven', email: 'steven@mail.test'}, sinon.match.func);
+      expect(record.id).to.equal(5);
+      expect(record.name).to.equal('Steven');
+      expect(record.email).to.equal('steven@mail.test');
     });
 
     it('should use default attributes if defined and not overwritten by the make arguments', async () => {
